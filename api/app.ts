9693c86fb87682e3d420826c430243faf8f816f0
James Wright <32226552+JamesWrightDev@users.noreply.github.com>
2023-12-16 12:19:42 +0000

    add task

--- api/app.ts
+++ api/app.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import serverlessExpress from '@vendia/serverless-express';
 import expressAsyncHandler from 'express-async-handler';
+import { DynamoDBClient, PutItemCommand } from '@aws-sdk/client-dynamodb';
+import { v4 } from 'uuid';
 
 const app = express();
 const router = express.Router();
@@ -9,7 +11,26 @@ app.use(router);
 app.post(
     '/task',
     expressAsyncHandler(async (req, res) => {
-        res.json({ message: 'hello world' });
+        const client = new DynamoDBClient({ region: 'eu-west-2' });
+        const ID = v4();
+        const command = new PutItemCommand({
+            TableName: 'tasks',
+            Item: {
+                taskId: {
+                    S: ID,
+                },
+                whatEverIWant: {
+                    S: 'SOME RANDOM TEXT',
+                },
+            },
+        });
+
+        try {
+            await client.send(command);
+            res.json({ message: `Task with ID ${ID} created successfully` });
+        } catch (e) {
+            res.json({ error: JSON.stringify(e) });
+        }
     }),
 );
 
