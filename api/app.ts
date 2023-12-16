a350c616ff24c66f1e41459ee7f2d228326dd378
Barryeasy90 <diams290@yahoo.fr>
2023-12-16 15:49:29 +0000

    post endpoint created

--- api/app.ts
+++ api/app.ts
@@ -1,26 +1,33 @@
 import express from 'express';
-import serverlessExpress from '@vendia/serverless-express';
+import serverlessExpress, { getCurrentInvoke } from '@vendia/serverless-express';
 import expressAsyncHandler from 'express-async-handler';
 import { DynamoDBClient, PutItemCommand } from '@aws-sdk/client-dynamodb';
 import { v4 } from 'uuid';
+import { request } from 'http';
+// import serverlessExpress, { getCurrentInvoke } from '@vendia/serverless-express';
 
 const app = express();
 const router = express.Router();
 app.use(router);
 
 app.post(
-    '/task',
+    '/tasks',
     expressAsyncHandler(async (req, res) => {
         const client = new DynamoDBClient({ region: 'eu-west-2' });
         const ID = v4();
+        const { event } = getCurrentInvoke();
+        const userId = event['requestContext']?.authorizer?.claims['cognito:username'];
         const command = new PutItemCommand({
             TableName: 'tasks',
             Item: {
                 taskId: {
                     S: ID,
                 },
-                whatEverIWant: {
-                    S: 'SOME RANDOM TEXT',
+                taskName:{
+                    S: req.body.taskName,
+                },
+                userid: {
+                    S: userId,
                 },
             },
         });
